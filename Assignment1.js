35ce6ed9786b03dc077b54bec5b3806b7ac47d28
Jesus Vicente Torresano M <jesusvicentetorresanom.@Jesuss-MacBook-Pro.local>
2017-11-27 11:42:37 +0100

    Initial commit

---
+++ Assignment1.js
@@ -0,0 +1,4 @@
+var img = document.createElement("img");
+img.src = "https://www.askideas.com/media/13/Welcome-Were-Glad-Youre-Here.jpg";
+var src = document.getElementById("script_img");
+src.appendChild(img);
